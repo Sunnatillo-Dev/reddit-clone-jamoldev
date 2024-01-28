5314460915598e5b933df918f3b7dcdda3d471b0
Sunnatillo-Dev <ruzmetovsunnatillo20081117@gmail.com>
2024-01-28 12:06:47 +0500

    added

---
+++ src/components/Navbar/SearchInput.tsx
@@ -0,0 +1,40 @@
+import { headerInputAtom } from '@/src/atoms/HeaderInout';
+import { Search2Icon } from '@chakra-ui/icons';
+import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import React from 'react';
+import { useRecoilState } from 'recoil';
+
+type SearchInputProps = {
+    user?: User | null;
+};
+
+const SearchInput:React.FC<SearchInputProps> = ({ user }) => {
+    const [inputValue, setInputValue] = useRecoilState(headerInputAtom)
+
+    return (
+        <Flex flexGrow={1} mr={2} maxW={'auto'} display={user ? 'flex' : 'none'}>
+             <InputGroup>
+                <InputLeftElement pointerEvents='none'>
+                  <Search2Icon color='gray.400' mb={1} />
+                </InputLeftElement>
+                <Input 
+                    placeholder='Search Reddit' 
+                    fontSize={'10pt'} 
+                    _placeholder={{ color: "gray.500" }} 
+                    _hover={{
+                        bg: "white",
+                        border: "1px solid blue.500"
+                    }}
+                    _focus={{
+                        outline: "none",
+                        border: "1px solid blue.500"
+                    }}
+                    height="34px"
+                    bg="gray.100"
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+             </InputGroup>
+        </Flex>
+    )
+}
+export default SearchInput;
